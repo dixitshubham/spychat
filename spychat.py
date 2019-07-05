3a4d8ac1152cdfb9b2946eb7679d1851f219d4bd
shubham dixit <shuhbhamdixit.17@gmail.com>
2019-07-05 10:46:47 +0530

    Initial commit

---
+++ spychat.py
@@ -0,0 +1,186 @@
+Main_Q =["read","clean dishes", "wash car"]
+from tkinter import *
+import os
+import tkinter as tk
+
+
+def delete2():
+    screen3.destroy()
+
+
+def delete3():
+    screen4.destroy()
+
+
+def delete4():
+    screen5.destroy()
+
+
+def add_friend():
+	global raw_filename
+	raw_filename=StringVar()
+	screen9 = Toplevel(screen)
+	screen9.title('ADD FRIEND')
+	screen9.geometry("300x250")
+	Label(screen9,text='SPY NAME' ).pack()
+	Entry(screen9,textvariable=raw_filename).pack()
+	Button(screen9,text='Search',command=save).pack()
+
+
+def save():
+	file=open('ram.txt')
+	data=file.read()
+	file.close()
+	screen10= Toplevel(screen)
+	screen10.title('ADD FRIEND')
+	screen10.geometry("300x250")
+	Result=Label(screen10,text=data)
+	Result.grid(row=1,column=1)
+
+
+def update_status():
+
+    screen11=Toplevel(screen)
+    screen11.title('you update status')
+    screen11.geometry("300x250")
+    Label(screen11,text='status').pack()
+
+
+def session():
+	screen8=Toplevel(screen)
+	screen8.title('WELCOME USER')
+	screen8.geometry("400x400")
+	Label(screen8,text='welcome to spy chat' ).pack()
+	Button(screen8,text='Add friend',command=add_friend).pack()
+	Button(screen8, text='Update status',command=update_status).pack()
+
+
+def login_sucess():
+
+    session()
+
+
+def password_not_recognised():
+    global screen4
+    screen4 = Toplevel(screen)
+    screen4.title("Success")
+    screen4.geometry("150x100")
+    Label(screen4, text="Password Error").pack()
+    Button(screen4, text="OK", command=delete3).pack()
+
+
+def user_not_found():
+    global screen5
+    screen5 = Toplevel(screen)
+    screen5.title("Success")
+    screen5.geometry("150x100")
+    Label(screen5, text="User Not Found").pack()
+    Button(screen5, text="OK", command=delete4).pack()
+
+
+def register_user():
+    print("working")
+
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(screen1, text="Registration Sucess", fg="green", font=("calibri", 11)).pack()
+
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+
+        else:
+            password_not_recognised()
+
+    else:
+        user_not_found()
+
+
+def register():
+    global screen1
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(screen1, text="Please enter details below").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username * ").pack()
+
+    username_entry = Entry(screen1, textvariable=username)
+    username_entry.pack()
+    Label(screen1, text="Password * ").pack()
+    password_entry = Entry(screen1, textvariable=password)
+    password_entry.pack()
+    Label(screen1, text="").pack()
+    Button(screen1, text="Register", width=10, height=1, command=register_user).pack()
+
+
+def login():
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("300x250")
+    Label(screen2, text="Please enter details below to login").pack()
+    Label(screen2, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_entry1
+    global password_entry1
+
+    Label(screen2, text="Username * ").pack()
+    username_entry1 = Entry(screen2, textvariable=username_verify)
+    username_entry1.pack()
+    Label(screen2, text="").pack()
+    Label(screen2, text="Password * ").pack()
+    password_entry1 = Entry(screen2, textvariable=password_verify)
+    password_entry1.pack()
+    Label(screen2, text="").pack()
+    Button(screen2, text="Login", width=10, height=1, command=login_verify).pack()
+
+
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("SPY CHAT")
+    Label(text="SPY CHAT", bg="grey", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Button(text="Login", height="2", width="30", command=login).pack()
+    Label(text="").pack()
+    Button(text="Register", height="2", width="30", command=register).pack()
+
+    screen.mainloop()
+
+
+main_screen()
